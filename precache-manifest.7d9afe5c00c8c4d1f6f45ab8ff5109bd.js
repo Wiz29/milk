6943e6b36ca14d2ed4ac2a38e675cb594872f19f
Wiz29 <rgodara29.rg@gmail.com>
2020-01-27 09:21:14 +0530

    Updates

---
+++ precache-manifest.7d9afe5c00c8c4d1f6f45ab8ff5109bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fbe48ced8ebe2e4cf5fd1779eedc58",
+    "url": "/milk/index.html"
+  },
+  {
+    "revision": "ab25578a036dd63c2c8f",
+    "url": "/milk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d359632cda84bdf8610",
+    "url": "/milk/static/js/2.e2045c47.chunk.js"
+  },
+  {
+    "revision": "84545ce239af0f818b69022fc2b83cdf",
+    "url": "/milk/static/js/2.e2045c47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab25578a036dd63c2c8f",
+    "url": "/milk/static/js/main.8e9fcadc.chunk.js"
+  },
+  {
+    "revision": "5b1eceee642690826496",
+    "url": "/milk/static/js/runtime-main.6236bbfe.js"
+  }
+]);
